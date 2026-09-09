a3d65f2d84f6ff7fe53bccfd3886ead3343286e4
izah1815 <carlaizabelly673@gmail.com>
2026-09-08 22:43:12 -0300

    Adiciona tela de Dashboard Regional

--- regional/src/routes/AppRoutes.tsx
+++ regional/src/routes/AppRoutes.tsx
@@ -1,15 +1,20 @@
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from 'react-router-dom';
+
 import MainLayout from '../layouts/MainLayout/MainLayout';
+
 import Login from '../pages/Login/Login';
 import EsqueciSenha from '../pages/EsqueciSenha/EsqueciSenha';
+import Dashboard from '../pages/Dashboard/Dashboard';
 import ClinicasPrivadas from '../pages/ClinicasPrivadas/ClinicasPrivadas';
 import Settings from '../pages/Settings/Settings';
 import ComingSoon from '../pages/ComingSoon/ComingSoon';
 
-// Rota de layout: tudo que for uma página "logada" é renderizada dentro
-// do MainLayout, através do <Outlet />. Como o MainLayout só é montado
-// uma vez para todo esse grupo de rotas, o estado do layout (sidebar,
-// menu mobile etc.) se mantém ao navegar entre as páginas.
 function AreaLogada() {
   return (
     <MainLayout>
@@ -22,19 +27,66 @@ function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/esqueci-senha" element={<EsqueciSenha />} />
 
+        {/* Login */}
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        {/* Esqueci minha senha */}
+        <Route
+          path="/esqueci-senha"
+          element={<EsqueciSenha />}
+        />
+
+        {/* Área logada */}
         <Route element={<AreaLogada />}>
-          <Route path="/clinicas-privadas" element={<ClinicasPrivadas />} />
-          <Route path="/dashboard" element={<ComingSoon title="Dashboard" />} />
-          <Route path="/ubs" element={<ComingSoon title="UBSs" />} />
-          <Route path="/estoque-regional" element={<ComingSoon title="Estoque Regional" />} />
-          <Route path="/configuracoes" element={<Settings />} />
+
+          {/* DASHBOARD */}
+          <Route
+            path="/dashboard"
+            element={<Dashboard />}
+          />
+
+          {/* Clínicas privadas */}
+          <Route
+            path="/clinicas-privadas"
+            element={<ClinicasPrivadas />}
+          />
+
+          {/* UBSs */}
+          <Route
+            path="/ubs"
+            element={<ComingSoon title="UBSs" />}
+          />
+
+          {/* Estoque Regional */}
+          <Route
+            path="/estoque-regional"
+            element={<ComingSoon title="Estoque Regional" />}
+          />
+
+          {/* Configurações */}
+          <Route
+            path="/configuracoes"
+            element={<Settings />}
+          />
+
         </Route>
 
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        {/* Página inicial */}
+        <Route
+          path="/"
+          element={<Navigate to="/login" replace />}
+        />
+
+        {/* Página não encontrada */}
+        <Route
+          path="*"
+          element={<Navigate to="/login" replace />}
+        />
+
       </Routes>
     </BrowserRouter>
   );
